08dad83c4c38f7f5339967d5c2986d5c6843c135
ONDA4PP <ondaredesocial@gmail.com>
2022-02-21 10:08:45 -0300

    branches

--- onda4pp/src/app/app.module.ts
+++ onda4pp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    CadastroComponent,
+    LoginComponent,
+    FaleConoscoComponent
   ],
   imports: [
     BrowserModule,
