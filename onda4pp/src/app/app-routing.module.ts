b8f080e151f2c0520baf27a9dd6a4c5bc813cb76
muriloperdigao <murilo.apm@gmail.com>
2022-02-24 09:38:31 -0300

    Junção das branchs e atualização 24.02.2022

--- onda4pp/src/app/app-routing.module.ts
+++ onda4pp/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo:"entrar", pathMatch:"full"},
+  {path:"entrar", component:LoginComponent},
+  {path: "cadastrar", component:CadastroComponent},
+  {path: "faleconosco", component:FaleConoscoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- onda4pp/src/app/app.module.ts
+++ onda4pp/src/app/app.module.ts
@@ -4,23 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
+import { Footer2Component } from './footer2/footer2.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent,
+    Footer2Component,
     CadastroComponent,
     LoginComponent,
-    FaleConoscoComponent
+    FaleConoscoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
